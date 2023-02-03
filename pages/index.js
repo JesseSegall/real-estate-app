bacfdbcdf3b49e975a7573923b6102c76a701c9d
Jesse <jsegall7@gmail.com>
2023-02-03 13:30:03 -0500

    Async function added to index

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
+import FeaturedProperties from '@/features/Home/components/FeaturedProperties';
 import HeroBanner from '@/features/Home/components/HeroBanner';
 import DefaultLayout from '@/features/Layouts/DefaultLayout';
 
-export default function Home() {
+export default function Home({ featuredProperties }) {
 	return (
 		<>
 			<DefaultLayout>
 				<HeroBanner />
+				<FeaturedProperties featuredProperties={featuredProperties} />
 			</DefaultLayout>
 		</>
 	);
+}
+
+// Fetches data at build time and generates a statically generated page
+export async function getStaticProps() {
+	const { hits } = require('@/features/data/properties');
+	return {
+		props: { featuredProperties: hits },
+	};
 }
