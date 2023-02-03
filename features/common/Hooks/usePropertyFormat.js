f1cbaf894ee11d9b268f2d1fcc3612ceda63e0e4
Jesse <jsegall7@gmail.com>
2023-02-03 14:35:34 -0500

    Formatter to get info out of data objects

---
+++ features/common/Hooks/usePropertyFormat.js
@@ -0,0 +1,38 @@
+// Any property passed into this will have the following components pulled from the data and returned in the object
+
+export const usePropertyFormat = (property) => {
+	const address = property.location.map((item) => item.name).join(', ');
+	const coverPhoto = property.coverPhoto.url;
+	const propertyType = `${(property.category[0].name, property.category[1].name)}`;
+	const price = property.price.toLocaleString('en-US', {
+		style: 'currency',
+		currency: 'USD',
+		maximumFractionDigits: 0,
+	});
+
+	const title = property.title;
+	const rooms = property.rooms;
+	const baths = property.baths;
+	const purpose = property.purpose;
+	const sqSize = property.sqSize;
+	const externalID = property.externalID;
+
+	const photos = property.photos?.map((photo) => photo.url) || [];
+	const description = property.description;
+	const coverVideoUrl = property.coverVideo.url;
+	const coverVideo = coverVideoUrl.slice(coverVideoUrl.length - 11);
+	const panorama = property.panoramas?.length ? property.panoramas[0].url : [];
+
+	return {
+		address,
+		coverPhoto,
+		propertyType,
+		price,
+		title,
+		rooms,
+		baths,
+		purpose,
+		sqSize,
+		externalID,
+	};
+};
