228f2ddd894e5b8f18b897a7ebf7c09ac8f5683c
Jesse <jsegall7@gmail.com>
2023-02-03 14:36:41 -0500

    Code for property slider

---
+++ features/Home/components/FeaturedProperties/components/PropertySlider/PropertySlider.js
@@ -0,0 +1,32 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination } from 'swiper';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import PropertyCard from '@/features/common/modules/PropertyCard';
+
+const PropertySlider = ({ featuredProperties }) => {
+	//console.log(featuredProperties);
+	return (
+		<Swiper
+			//slidesPerView={isDesktop ? 3 : 1}
+			spaceBetween={10}
+			loop={true}
+			loopFillGroupWithBlank={true}
+			centeredSlides={true}
+			autoplay={{ delay: 2000, disableOnInteraction: true }}
+			pagination={{ dynamicBullets: true }}
+			modules={[Autoplay, Pagination]}
+			className='mySwiper'
+		>
+			{featuredProperties.map((property) => (
+				<SwiperSlide key={property.id}>
+					<PropertyCard {...property} />
+				</SwiperSlide>
+			))}
+		</Swiper>
+	);
+};
+
+export default PropertySlider;
