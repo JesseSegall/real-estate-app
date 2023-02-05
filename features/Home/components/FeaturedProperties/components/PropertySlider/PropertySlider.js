eb9558b4a9794139049fa249efc62a14f080b328
Jesse <jsegall7@gmail.com>
2023-02-05 17:04:15 -0500

    Updated for mobile

--- features/Home/components/FeaturedProperties/components/PropertySlider/PropertySlider.js
+++ features/Home/components/FeaturedProperties/components/PropertySlider/PropertySlider.js
@@ -5,13 +5,13 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import PropertyCard from '@/features/common/modules/PropertyCard';
+import { useIsDesktop } from '@/features/common/Hooks/useIsDesktop';
 
 const PropertySlider = ({ featuredProperties }) => {
-	//console.log(featuredProperties);
+	const { isDesktop } = useIsDesktop();
 	return (
 		<Swiper
-			//slidesPerView={isDesktop ? 3 : 1}
-			slidesPerView={3}
+			slidesPerView={isDesktop ? 3 : 1}
 			spaceBetween={10}
 			loop={true}
 			loopFillGroupWithBlank={true}
